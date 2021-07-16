8d488288e353857f1980cb1adeeb8cc8085ce31f
Dusan Orlovic <duleorlovic@gmail.com>
2021-07-16 13:16:40 +0200

    Template to install hotwire and bootstrap, generate todo scaffold

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module TrkRailsFormsHotwireTodoAppBootstrapModal
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.jbuilder false
+      generate.helper false
+      generate.stylesheets false
+      generate.test_framework nil
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
